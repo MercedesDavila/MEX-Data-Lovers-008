f75e955eb8d555ceb1f806329c1a0fba6c04e378
MercedesDavila <mmdavila@outlook.com>
2019-07-09 18:17:47 -0500

    Se agrego funcion filtrar por nombre

--- src/data.js
+++ src/data.js
@@ -3,7 +3,32 @@
 
 
 //const example = () => {
-  //return 'example';
+//return 'example';
 //};
 
 //window.example = example;
+
+
+// Filter A-Z Y  Z-A
+// Función para ordenar los pokemones por nombre 
+
+const sorData = (dataPokemon, sortBy, sortOrder) => {
+    let pokemonOrder = [];
+    switch (sortOrder) {
+        case 'ASC': //  Ordenar de manera ascedente 
+            if (sortBy === 'name') {
+                pokemonOrder = dataPokemon.sort((a, b) => (a.name > b.name ? 1 : -1));
+            }
+            break;
+
+        case 'DESC': // Ordenar de manera descendente
+            if (sortBy === 'name') {
+                pokemonOrder = dataPokemon.sort((a, b) => (a.name < b.name ? 1 : -1));
+            }
+            break;
+
+    }
+    return pokemonOrder;
+};
+
+window.sorData = sorData;
--- test/data.spec.js
+++ test/data.spec.js
@@ -1,12 +1,9 @@
-require('../src/data.js');
-
-
 describe('example', () => {
-  it('is a function', () => {
-    expect(typeof example).toBe('function');
-  });
+    it('is a function', () => {
+        expect(typeof example).toBe('function');
+    });
 
-  it('returns `example`', () => {
-    expect(example()).toBe('example');
-  });
+    it('returns `example`', () => {
+        expect(example()).toBe('example');
+    });
 });
